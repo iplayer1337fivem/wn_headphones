16ab65acf8a73244f8bb344a8bd04271c6426312
ARottenberger <69944020+iplayer1337fivem@users.noreply.github.com>
2024-07-13 04:42:36 +0200

    danglish

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config = {}
-Config.Framework = "ESX" -- ESX or qbcore
+Config.Framework = "qbcore" -- ESX or qbcore
 Config.Target = "ox_target" -- ox_target, qb-target
 Config.Notify = "ox_lib" -- ox_lib, qbcore or ESX
 Config.Duration = 6000 -- How long will the player search the pleace IN MS
@@ -8,7 +8,7 @@ Config.Item = "bigheadphones"
 Config.Props = 'h4_prop_battle_headphones_dj'
 
 Config.Language = {
-    ['no_item'] = "You dont have headphones",
-    ['target'] = "Put on/down headphones on a player",
-    ['progress'] = "Putting headphonse on/down",
+    ['no_item'] = "Du mangler hovedtelefoner",
+    ['target'] = "Giv/Fjern hovedtelefoner fra person",
+    ['progress'] = "Håndterer hovedtelefonerne",
 }
